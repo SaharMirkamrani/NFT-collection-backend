bde989a16927f00865da09d2f53727116ce51ed3
Sahar Mirkamrani <shrmrkmrn@gmail.com>
2022-07-15 16:31:43 +0430

    Initial Commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+
+const WHITELIST_CONTRACT_ADDRESS = "0xeE312687e03401Cf4403BAF0226fA4B4e992aa40";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
